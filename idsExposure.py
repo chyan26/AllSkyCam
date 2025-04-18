b071bb70eeb12b091c5899612024101a187bf888
Chi-Hung Yan <chyan26@gmail.com>
2025-04-18 15:40:10 +0800

    Commit a working version.

--- idsExposure.py
+++ idsExposure.py
@@ -971,10 +971,10 @@ class SharedState:
 def parse_args():
     """Parses command-line arguments."""
     parser = argparse.ArgumentParser(description="IDS Peak Camera Acquisition Script")
-    parser.add_argument("--exposure", type=float, default=0.02, help="Exposure time in milliseconds")
-    parser.add_argument("--images", type=int, default=100, help="Number of images to acquire")
-    parser.add_argument("--sleep", type=int, default=0, help="time of seconds between exposures")
-    parser.add_argument("--buffers", type=int, default=None, help="Number of buffers to allocate")
+    parser.add_argument("--exposure", type=float, default=0.2, help="Exposure time in milliseconds")
+    parser.add_argument("--images", type=int, default=5, help="Number of images to acquire")
+    parser.add_argument("--sleep", type=float, default=0.1, help="Time in seconds between exposures")
+    parser.add_argument("--buffers", type=int, default=None, help="Number of buffers to allocate (default: minimum required)")
     parser.add_argument("--output", type=str, default="output", help="Directory to save FITS files")
     parser.add_argument("--perform_analysis", action='store_true', help="Set this flag to perform sun analysis on the images")
     parser.add_argument("--gps_update_frequency", type=float, default=10, help="Frequency of GPS updates in Hz")
